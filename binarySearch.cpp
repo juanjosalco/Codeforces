476bacdcbe9431b949971dfd9c02b3b77e252134
Juan Salazar <jjsc2003@live.com>
2022-08-16 23:32:46 -0500

    Binary Search Standart Method

--- binarySearch.cpp
+++ binarySearch.cpp
@@ -4,22 +4,33 @@ using namespace std;
 
 int main(){
     int n, k;
-    int tmp;
-    vector<int> nums;
     cin >> n >> k;
+    vector<int> nums(n);
     for(int i = 0; i < n; i++){
-        cin >> tmp;
-        nums.push_back(tmp);
+        cin >> nums[i];
     }
-    
     for(int i = 0; i < k; i++){
-        int l = 0; 
-        int r = nums.size()-1;
-        while(l >= r){
-
+        int x;
+        cin >> x;
+        int l = 0;
+        int r = n - 1;
+        bool found = false;
+        while(r >= l){
+            int mid = (l + r) / 2;
+            if(nums[mid] == x){
+                found = true;
+                break;
+            } else if( nums[mid] < x){
+                l = mid + 1;
+            } else{
+                r = mid - 1;
+            }
+        }
+        if(found){
+            cout << "YES" << endl;
+        } else {
+            cout << "NO" << endl;
         }
     }
-    cout << nums[9];
-
-
+    return 0;
 }
